885ecbb87173846dd684a063989da91e9438d36d
helloGithub326 <pranay.aluri@gmail.com>
2024-11-10 15:53:02 -0600

    Changed some things in index.html

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var btn1 = document.getElementById("btn1");
-var btn2 = document.getElementById("btn2");
-var btn3 = document.getElementById("btn3");
-var btn4 = document.getElementById("btn4");
+var btn1 = document.getElementById("#btn1");
+var btn2 = document.getElementById("#btn2");
+var btn3 = document.getElementById("#btn3");
+var btn4 = document.getElementById("#btn4");
 
 //Hover
 function btn1Hover() {
