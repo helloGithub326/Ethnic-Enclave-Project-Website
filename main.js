619621e88c3e67b80fd9167d9e7e4977f0bb60ed
helloGithub326 <pranay.aluri@gmail.com>
2024-11-10 15:41:24 -0600

    Directly added function for btn1

--- main.js
+++ main.js
@@ -4,20 +4,20 @@ btn3 = document.getElementById("btn3");
 btn4 = document.getElementById("btn4");
 
 //Hover
-btn1.addEventListener("mouseover", () => {
+btn1Hover = function() {
     btn1.querySelector("#btn1T").innerText = "Topic 1 Description"
+}
+
+btn2.addEventListener("mouseover", () => {
+    btn2.querySelector("#btn2T").innerText = "Topic 2 Description"
 });
 
-btn1.addEventListener("mouseover", () => {
-    btn1.querySelector("#btn2T").innerText = "Topic 2 Description"
+btn3.addEventListener("mouseover", () => {
+    btn3.querySelector("#btn3T").innerText = "Topic 3 Description"
 });
 
-btn1.addEventListener("mouseover", () => {
-    btn1.querySelector("#btn3T").innerText = "Topic 3 Description"
-});
-
-btn1.addEventListener("mouseover", () => {
-    btn1.querySelector("#btn4T").innerText = "Topic 4 Description"
+btn4.addEventListener("mouseover", () => {
+    btn4.querySelector("#btn4T").innerText = "Topic 4 Description"
 });
   
 //Hover off
@@ -25,14 +25,14 @@ btn1.addEventListener("mouseout", () => {
     btn1.querySelector("#btn1T").innerText = "Topic 1"
 });
 
-btn1.addEventListener("mouseout", () => {
-    btn1.querySelector("#btn2T").innerText = "Topic 2"
+btn2.addEventListener("mouseout", () => {
+    btn2.querySelector("#btn2T").innerText = "Topic 2"
 });
 
-btn1.addEventListener("mouseout", () => {
-    btn1.querySelector("#btn3T").innerText = "Topic 3"
+btn3.addEventListener("mouseout", () => {
+    btn3.querySelector("#btn3T").innerText = "Topic 3"
 });
 
-btn1.addEventListener("mouseout", () => {
-    btn1.querySelector("#btn4T").innerText = "Topic 4"
+btn4.addEventListener("mouseout", () => {
+    btn4.querySelector("#btn4T").innerText = "Topic 4"
 });
