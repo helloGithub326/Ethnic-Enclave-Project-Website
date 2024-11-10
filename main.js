8ce99221218ef8ba51dd4e1af062e600848077a7
helloGithub326 <pranay.aluri@gmail.com>
2024-11-10 15:24:24 -0600

    Added button variables

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 btn1 = document.getElementById("btn1");
+btn2 = document.getElementById("btn2");
+btn3 = document.getElementById("btn3");
+btn4 = document.getElementById("btn4");
 
 //Hover
 btn1.addEventListener("mouseover", () => {
