bdefb80315a6eb66a404091945464fe5fd68f042
helloGithub326 <pranay.aluri@gmail.com>
2024-11-10 22:06:45 -0600

    Finished topic2

--- topic1.js
+++ topic1.js
@@ -1,11 +1,11 @@
 var captions = {
-    "streetMap": "A street map of Chicago Chinatown",
-    "satMap": "A satellite map of Chicago Chinatown",
-    "raceMap": "A map of Chicago Chinatown showing the majority race of neighborhoods",
-    "lc1": "People in Chicago Chinatown celebrating Chinese Lunar New Year",
-    "lc2": "The architecture that Chinatown is made up of, which greatly differs from the architecture of other places in Chicago",
-    "pc1": "A walgreens in Chinatown shows pop culture as Walgreens pharmacies are seen widely across the world",
-    "pc2": "The architecture in the picture shown of the hotel represents the modern architecture we have today that is seen all around the world"
+    "streetMap": "A street map of Chicago Chinatown.",
+    "satMap": "A satellite map of Chicago Chinatown.",
+    "raceMap": "A map of Chicago Chinatown showing the majority race of neighborhoods.",
+    "lc1": "The picture shows people in Chicago Chinatown celebrating Chinese Lunar New Year.",
+    "lc2": "The picture shows the architecture that Chinatown is made up of, which greatly differs from the architecture of other places in Chicago.",
+    "pc1": "The picture shows a walgreens in Chinatown that resembles pop culture since Walgreens pharmacies are seen widely across the world.",
+    "pc2": "The picture shows the architecture of the hotel, which represents the modern architecture we have today that is seen all around the world."
 }
 
 logo = document.getElementById("logo");
--- topic2.js
+++ topic2.js
@@ -1,5 +1,7 @@
 var captions = {
-    
+    "lcl1": "The picture shows the clearly depicted in Mandarin entrance sign of Chinatown.",
+    "lcl2": "The picture shows signs for shops written in Mandarin which typically would not be shown that way elsewhere.",
+    "lcl3": "The picture shows street names in Chinatown written in Mandarin and English, which is not common in other places."
 }
 
 logo = document.getElementById("logo");
