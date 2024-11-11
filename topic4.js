f7f645a8da1365ca04c320f3006a4b6a72e93e66
helloGithub326 <145622254+helloGithub326@users.noreply.github.com>
2024-11-11 15:29:03 -0600

    Changed captions for topic4

--- topic4.js
+++ topic4.js
@@ -1,9 +1,9 @@
 var captions = {
     "distEthnicBtn": "A map showing the Asian percentage in neighborhoods including Chicago Chinatown.",
-    "ecl1": "The picture shows a building built with Chinese architecture, which depicts Chinese cultural beliefs.",
-    "ecl2": "The picture shows a dragon, which is a symbolic to Chinese culture.",
-    "gcl1": "The picture shows a Buddhist temple, which have different gender spaces and roles.",
-    "gcl2": "The picture shows a salon in Chicago Chinatown that is specifically for females."
+    "ecl1": "The picture shows a building built with Chinese architecture, which depicts Chinese cultural beliefs. (Ethnic Enclave Impact on The Cultural Landscape)",
+    "ecl2": "The picture shows a dragon, which is a symbolic to Chinese culture. (Ethnic Enclave Impact on The Cultural Landscape)",
+    "gcl1": "The picture shows a Buddhist temple, which have different gender spaces and roles. (Gender Impact on The Cultural Landscape)",
+    "gcl2": "The picture shows a salon in Chicago Chinatown that is specifically for females. (Gender Impact on The Cultural Landscape)"
 }
 
 logo = document.getElementById("logo");
